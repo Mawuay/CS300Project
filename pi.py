7823e7f0239b5b6de2f90aadc28cb7d3040d9134
Mawuay <dackuaku@gmail.com>
2020-05-03 23:30:59 -0400

    pi.py youtube tutorial

--- pi.py
+++ pi.py
@@ -11,12 +11,9 @@
 # intercaes with the google home.   
 #############################################################################################
 from __future__ import print_function 
-# from googleapiclient.discovery import build  
-# from httplib2 import Http  
-from oauth2client import file, client, tools  
+import gspread 
 from oauth2client.service_account import ServiceAccountCredentials
 import serial
-import gspread
 import time 
 import datetime
 
