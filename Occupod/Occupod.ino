fe94c8e665cb3495b02ab65752cb4998086f21f1
Mawuay <dackuaku@gmail.com>
2020-05-06 02:43:25 -0400

    Final commit before submission

--- Occupod/Occupod.ino
+++ Occupod/Occupod.ino
@@ -20,14 +20,13 @@
  * 
  * Hardware: - Arduino mega
  *           - Raspberry Pi 3 B+
- *           - LCD 16 x 2 character display
  *           - Temperature and Humidity Sensor 
  *           - Motion sensor 
  *           - Light Sensor 
  *                    
  * 
- * IoT onfrustructure: - SQLite
- *                     - Some other stuff (See lab 8).
+ * IoT onfrustructure: - Google Sheets
+ *                     
  *                     
  **************************************************************************************************************************
  */
@@ -55,14 +54,6 @@ int sensorValue = 0; // variable to store the value coming from the sensor
 DHT TEMPHUMSENSOR(DHTPIN, DHTTYPE);
 
 
-/** Function Definition **/
-//int detectTemperature();
-//int detectHumidity();
-//unsigned char dectectMotion();
-//unsigned char dectectLights();
-//unsigned char detectPresence();
-
-
 
 /** Function Declaration **/
 /* 
@@ -72,9 +63,7 @@ DHT TEMPHUMSENSOR(DHTPIN, DHTTYPE);
  */
 int detectTemperature(){
   int bathroomTemperature = TEMPHUMSENSOR.readTemperature();
-//  Serial.print("Temperature: ");
   Serial.print(bathroomTemperature);
-//  Serial.print("°C");
   Serial.print("\t"); 
   return bathroomTemperature;
  }
@@ -87,11 +76,8 @@ int detectTemperature(){
  */
 int detectHumidity(){
   int bathroomHumidity = TEMPHUMSENSOR.readHumidity();
-// Serial.print("Humidity: ");
  Serial.print(bathroomHumidity);
-// Serial.print(" %");
  Serial.print("\t");
-// Serial.print("\n");
   return bathroomHumidity; 
  }
 
@@ -103,10 +89,8 @@ int detectHumidity(){
 unsigned detectMotion(){
    VALUE = digitalRead(MOTIONSEN);       // read input value
   if (VALUE == MOTIONSTATE) {            // check if the input is HIGH
-//     Serial.print("Motion: ");
      Serial.println(VALUE);
   } else {
-//     Serial.print("Motion: ");
      Serial.println(VALUE);
   } 
   return VALUE;
@@ -120,41 +104,27 @@ unsigned detectMotion(){
  */
 int detectLights(){
   analogValueOfLights = analogRead(lightSensorPin);
-//  Serial.print("Light intensity: " );
   Serial.print(analogValueOfLights);
   Serial.print("\t");
   return analogValueOfLights;
 }
 
 
-
-
-
-
 /********************************* Main code ***********************************/
 void setup() {
-  // put your setup code here, to run once:
   TEMPHUMSENSOR.begin();
   pinMode(MOTIONSEN, INPUT);     // declare sensor as input
   pinMode(lightSensorPin, INPUT);
-  pinMode(buttonpin,INPUT);//define switch as a output port 
+  pinMode(buttonpin,INPUT);      //define switch as a output port 
   Serial.begin(9600);
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  //delay(5000);
+  // Wait a ten seconds between measurements.
   int  light = detectLights();
   int temp = detectTemperature();
   int humid = detectHumidity();
   unsigned motion = detectMotion();
-  // if (detectLights() >= 20 && detectHumidity() >= 50 && detectTemperature() >= 20) {
-  //  Serial.println("Shower");
-  // } else if (detectLights() >= 20 && detectHumidity() <= 50 && detectMotion() > 0){
-  //  Serial.println("Toilet");
-  // } else {
-  //  Serial.println("Vaccant"); 
-  // }
-  delay(10000);               // 60 sec delay...
+  delay(10000);               
   
 }
--- pi.py
+++ pi.py
@@ -3,12 +3,11 @@
 # Date : 2nd May 2020
 # Written by: Daniel Ackuaku, for CS 300 Final Project
 # Institution: Calvin University
-# Professor: Dereck Schuurman
-#
+# Professor: Derek Schuurman
 #
 # Reads serial data from the sensors on the arduino and stores them in an google sheets
-# uses data from the google sheets to detect if the bathroom is occupied or not.
-# intercaes with the google home.   
+# uses data from the google sheets to detect if the bathroom is occupied or not
+# deletes data from teh google sheet after 1 hour
 #############################################################################################
 from pprint import pprint
 import gspread 
@@ -18,39 +17,65 @@ import time
 import datetime
 
 ##########################################################################
+# Configuring credentials for the google sheet API
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", SCOPES)
 client = gspread.authorize(creds)
+
+# Opening the specific sheets in the google sheets workbook
 sheet = client.open("Occupod").sheet1
 sheet2 = client.open("Occupod").worksheet('BathroomState')
-
-
-
 ##########################################################################
+# Variable definitions 
 
+countTimer = 0
+
+
+
+# Function definition
+# Function that reads in serial data, parses it into strings and publishes it to google sheets
+def sense_and_record():
+	global countTimer
+	arduino = serial.Serial("/dev/ttyACM0", 9600) # Reads in the sensor data
+	data = arduino.readline().decode() # Decodes that data into a string
+	pieces = data.split("\t") # splits the string into the parts of infromation required
+	lights = pieces[0]
+	temperature = pieces[1]
+	humidity = pieces[2]
+	motion = pieces[3]
+	currentTime = datetime.datetime.now() # sets the currentTime variable to local time
+	# Algorithm that detemines if someone is in the bath room and what activity they might be doing based on sensor data & updates the sheet
+	if (lights >="20" and humidity >= "50" and temperature >= "20"):
+		bathroomState = [currentTime.strftime("%X"), "Occupied - Shower"]
+		sheet2.append_row(bathroomState)
+	elif int(lights) >= 20 and int(humidity) <= 40 and int(motion) > 0:
+		bathroomState = [currentTime.strftime("%X"), "Occupied - Toilet"]
+		sheet2.append_row(bathroomState)
+	else:
+		bathroomState = [currentTime.strftime("%X"), "Vacant"]
+		sheet2.append_row(bathroomState)
+
+	appendRow = [currentTime.strftime("%X"), int(lights), float(temperature) , int(humidity) ,int(motion)]
+	sheet.append_row(appendRow)
+	countTimer = countTimer + 1
+
+
+# Function that deletes from the sheet after 1 hour of sensing
+def delete_and_update():
+	global countTimer
+	if (countTimer == 360):
+		for x in range (2,360):
+			sheet.delete_row(x)
+			sheet2.delete_row(x)
+		countTimer = 0
+
+# Continus loop that reads data from the Arduino sensors 
 try:
-	print("Welcome you are updating the googel sheet.")
+	print("Welcome you are updating the google sheet.")
 	while True:
-		arduino = serial.Serial("/dev/ttyACM0", 9600) # make sure you write correct serial Pi -"/dev/ttyACM0" Mac -/dev/cu.usbmodem14101
-		data = arduino.readline().decode()
-		pieces = data.split("\t")
-		lights = pieces[0]
-		temperature = pieces[1]
-		humidity = pieces[2]
-		motion = pieces[3]
-		currentTime = datetime.datetime.now()
-		if (lights >="20" and humidity >= "50" and temperature >= "20"):
-			bathroomState = [currentTime.strftime("%X"), "Occupied - Shower"]
-			sheet2.append_row(bathroomState)
-		elif int(lights) >= 20 and int(humidity) <= 40 and int(motion) > 0:
-			bathroomState = [currentTime.strftime("%X"), "Occupied - Toilet"]
-			sheet2.append_row(bathroomState)
-		else:
-			bathroomState = [currentTime.strftime("%X"), "Vaccant"]
-			sheet2.append_row(bathroomState)
+		sense_and_record()
+		delete_and_update()
 
-		appendRow = [currentTime.strftime("%X"), int(lights), float(temperature) , (humidity) ,motion]
-		sheet.append_row(appendRow)
 except KeyboardInterrupt:
 	print ("Done")
 
